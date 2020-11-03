745901750649bb9e95d173233c024e4e3194d60a
Michael Troia <troia.michael.a@gmail.com>
2020-11-03 11:22:17 -0600

    Servlets exercise: Hello World with Tomcat

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/counter")
+public class CounterServlet {
+    private int number = 0;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        number++;
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("number: " + number);
+    }
+}
